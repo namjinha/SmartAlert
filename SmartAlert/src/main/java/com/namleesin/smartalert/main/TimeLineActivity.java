6a520b750137cbdff26da572dffcbac619f942a9
kukuru <comus1200@gmail.com>
2015-12-24 18:07:49 +0900

    update

---
+++ SmartAlert/src/main/java/com/namleesin/smartalert/main/TimeLineActivity.java
@@ -0,0 +1,17 @@
+package com.namleesin.smartalert.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by chitacan on 2015. 12. 24..
+ */
+public class TimeLineActivity extends Activity{
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
